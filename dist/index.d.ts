5a5d50b883021884d88324a790d304101508131c
Ali Amirnezhad <webilix@gmail.com>
2023-02-17 00:21:13 +0330

    4.2.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -135,7 +135,7 @@ export declare const Helper: {
         toString: (date: Date) => string;
     };
     NUMBER: {
-        format: (num: number, locale?: "FA" | "EN") => string;
+        format: (num: number, locale?: "EN" | "FA") => string;
         toEN: (num: string | number) => string;
         toFA: (num: string | number) => string;
         toFileSize: (size: number, english?: boolean) => string;
@@ -159,7 +159,7 @@ export declare const Helper: {
         toString: (plate: string[], join?: string) => string | null;
     };
     STRING: {
-        changeNumbers: (str: string, changeTo?: "FA" | "EN") => string;
+        changeNumbers: (str: string, changeTo?: "EN" | "FA") => string;
         escapeHTML: (str: string) => string;
         getBankCardView: (bankCard: string, join?: string) => string;
         getFileName: (name: string, ext: string, seperator?: string) => string;
