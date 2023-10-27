3dbc71f99888b8b38bf17aa3cfd8a8a743306353
Ali Amirnezhad <webilix@gmail.com>
2023-10-27 12:52:03 +0330

    Refactor GEO class

--- dist/lib/helpers/geo.d.ts
+++ dist/lib/helpers/geo.d.ts
@@ -1,12 +1,15 @@
 import { IGeoCoordinates } from '../shared';
+declare function coordinates(): Promise<IGeoCoordinates>;
+declare function distance(from: {
+    latitude: number;
+    longitude: number;
+}, to: {
+    latitude: number;
+    longitude: number;
+}): number;
 export declare const GEO: {
-    coordinates: () => Promise<IGeoCoordinates>;
-    distance: (from: {
-        latitude: number;
-        longitude: number;
-    }, to: {
-        latitude: number;
-        longitude: number;
-    }) => number;
+    coordinates: typeof coordinates;
+    distance: typeof distance;
 };
+export {};
 //# sourceMappingURL=geo.d.ts.map
--- src/lib/helpers/geo.ts
+++ src/lib/helpers/geo.ts
@@ -1,39 +1,41 @@
 import { IGeoCoordinates } from '../shared';
 
-export const GEO = {
-    coordinates: (): Promise<IGeoCoordinates> => {
-        return new Promise<IGeoCoordinates>((resolve, reject) => {
-            try {
-                if (!navigator || !navigator.geolocation) {
-                    reject('Geolocation is not supported by this browser.');
-                    return;
-                }
-
-                navigator.geolocation.getCurrentPosition(
-                    (response: GeolocationPosition) =>
-                        resolve({
-                            latitude: response.coords.latitude,
-                            longitude: response.coords.longitude,
-                        }),
-                    (err: GeolocationPositionError) => reject(err.message),
-                );
-            } catch (e: any) {
-                reject(e.message || 'navigator is not defined');
+function coordinates(): Promise<IGeoCoordinates> {
+    return new Promise<IGeoCoordinates>((resolve, reject) => {
+        try {
+            if (!navigator || !navigator.geolocation) {
+                reject('Geolocation is not supported by this browser.');
+                return;
             }
-        });
-    },
 
-    distance: (from: { latitude: number; longitude: number }, to: { latitude: number; longitude: number }): number => {
-        const R = 6371e3; // metres
-        const φ1 = (from.latitude * Math.PI) / 180; // φ, λ in radians
-        const φ2 = (to.latitude * Math.PI) / 180;
-        const Δφ = ((to.latitude - from.latitude) * Math.PI) / 180;
-        const Δλ = ((to.longitude - from.longitude) * Math.PI) / 180;
+            navigator.geolocation.getCurrentPosition(
+                (response: GeolocationPosition) =>
+                    resolve({
+                        latitude: response.coords.latitude,
+                        longitude: response.coords.longitude,
+                    }),
+                (err: GeolocationPositionError) => reject(err.message),
+            );
+        } catch (e: any) {
+            reject(e.message || 'navigator is not defined');
+        }
+    });
+}
 
-        const a =
-            Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+function distance(from: { latitude: number; longitude: number }, to: { latitude: number; longitude: number }): number {
+    const R = 6371e3; // metres
+    const φ1 = (from.latitude * Math.PI) / 180; // φ, λ in radians
+    const φ2 = (to.latitude * Math.PI) / 180;
+    const Δφ = ((to.latitude - from.latitude) * Math.PI) / 180;
+    const Δλ = ((to.longitude - from.longitude) * Math.PI) / 180;
 
-        return Math.round(R * c); // in metres
-    },
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return Math.round(R * c); // in metres
+}
+
+export const GEO = {
+    coordinates: coordinates,
+    distance: distance,
 };
